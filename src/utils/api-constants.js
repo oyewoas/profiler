6dff5ddf823879aa511a1ff418e44b606a84dff5
Ayooluwa Oyewo <ayooluwaoyewo@gmail.com>
2020-02-03 17:56:04 +0100

    added the start script for deploy to heroku and added the live rest api endpoint url

--- src/utils/api-constants.js
+++ src/utils/api-constants.js
@@ -1,5 +1,4 @@
-// const API_BASE_URL = 'https://jetcakeapi.herokuapp.com/api/v1'
-const API_BASE_URL = 'http://localhost:4000/api/v1'
+const API_BASE_URL = 'https://jetcakeapi.herokuapp.com/api/v1'
 const LOGIN_API = `${API_BASE_URL}/user/signin`
 const SIGN_UP_API = `${API_BASE_URL}/user/signup`
 const GET_PROFILE = `${API_BASE_URL}/user/profile`
